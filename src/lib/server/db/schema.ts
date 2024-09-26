231b5433acdc9bfd8b1fdb9c11131db7845521ca
Johannes Andersson <hello@thejoltjoker.com>
2024-09-27 00:39:55 +0200

    feat: show linked data in categories view

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,20 +1,23 @@
 import { relations, type InferInsertModel, type InferSelectModel } from 'drizzle-orm';
 import { integer, pgEnum, pgTable, primaryKey, serial, text, timestamp } from 'drizzle-orm/pg-core';
-export const roleEnum = pgEnum('role', ['user', 'admin']);
 
+// General
+export const roleEnum = pgEnum('role', ['user', 'admin']);
+export const mediaTypeEnum = pgEnum('mediaType', ['image', 'video', 'audio']);
+
+// Users
 export const users = pgTable('user', {
 	id: text('id')
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
 	username: text('username').unique(),
 	email: text('email').unique(),
-	emailVerifiedAt: timestamp('emailVerifiedAt', { mode: 'date' }),
+	emailVerifiedAt: timestamp('email_verified_at', { mode: 'date' }),
 	password: text('password'),
 	role: roleEnum('role').default('user'),
 	image: text('image'),
-	ticket: text('ticket'),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -23,21 +26,24 @@ export const usersRelations = relations(users, ({ many }) => ({
 	accounts: many(accounts),
 	submissions: many(submissions),
 	votes: many(votes),
-	reactions: many(reactions)
+	reactions: many(reactions),
+	tickets: many(tickets)
 }));
 
 export type User = InferSelectModel<typeof users>;
 
+// Accounts
+
 export const accounts = pgTable(
 	'account',
 	{
-		userId: text('userId')
+		userId: text('user_id')
 			.notNull()
 			.references(() => users.id, { onDelete: 'cascade' }),
 		provider: text('provider').notNull(),
-		providerAccountId: text('providerAccountId').notNull(),
-		createdAt: timestamp('createdAt').defaultNow(),
-		updatedAt: timestamp('updatedAt')
+		providerAccountId: text('provider_account_id').notNull(),
+		createdAt: timestamp('created_at').defaultNow(),
+		updatedAt: timestamp('updated_at')
 			.defaultNow()
 			.$onUpdate(() => new Date())
 	},
@@ -56,13 +62,13 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
 }));
 
 export const sessions = pgTable('session', {
-	sessionToken: text('sessionToken').primaryKey(),
-	userId: text('userId')
+	sessionToken: text('session_token').primaryKey(),
+	userId: text('user_id')
 		.notNull()
 		.references(() => users.id, { onDelete: 'cascade' }),
-	expiresAt: timestamp('expiresAt', { mode: 'date' }).notNull(),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	expiresAt: timestamp('expires_at', { mode: 'date' }).notNull(),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -70,16 +76,18 @@ export const sessions = pgTable('session', {
 export type Session = InferSelectModel<typeof sessions>;
 export type InsertSession = InferInsertModel<typeof sessions>;
 
+// Events
+
 export const events = pgTable('event', {
 	id: serial('id').primaryKey(),
 	name: text('name').notNull(),
 	description: text('description'),
-	submissionsOpenAt: timestamp('submissionsOpenAt', { mode: 'date' }).notNull(),
-	submissionsCloseAt: timestamp('submissionsCloseAt', { mode: 'date' }).notNull(),
-	votingOpenAt: timestamp('votingOpenAt', { mode: 'date' }).notNull(),
-	votingCloseAt: timestamp('votingCloseAt', { mode: 'date' }).notNull(),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	submissionsOpenAt: timestamp('submissions_open_at', { mode: 'date' }).notNull(),
+	submissionsCloseAt: timestamp('submissions_close_at', { mode: 'date' }).notNull(),
+	votingOpenAt: timestamp('voting_open_at', { mode: 'date' }).notNull(),
+	votingCloseAt: timestamp('voting_close_at', { mode: 'date' }).notNull(),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -88,15 +96,41 @@ export type Event = InferSelectModel<typeof events>;
 
 export const eventsRelations = relations(events, ({ many }) => ({
 	categoriesToEvents: many(categoriesToEvents),
-	submissions: many(submissions)
+	submissions: many(submissions),
+	tickets: many(tickets)
 }));
+
+export const tickets = pgTable('tickets', {
+	id: serial('id').primaryKey(),
+	content: text('content'),
+	userId: text('user_id'),
+	eventId: integer('event_id'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
+		.defaultNow()
+		.$onUpdate(() => new Date())
+});
+
+export const ticketsRelations = relations(tickets, ({ one }) => ({
+	user: one(users, {
+		fields: [tickets.userId],
+		references: [users.id]
+	}),
+	event: one(events, {
+		fields: [tickets.eventId],
+		references: [events.id]
+	})
+}));
+
+// Categories
 
 export const categories = pgTable('category', {
 	id: serial('id').primaryKey(),
 	name: text('name').notNull(),
 	description: text('description'),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	allowedMediaType: mediaTypeEnum('allowed_media_type'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -105,6 +139,11 @@ export const categoriesRelations = relations(categories, ({ many }) => ({
 	categoriesToEvents: many(categoriesToEvents),
 	submissions: many(submissions)
 }));
+
+export type Category = InferSelectModel<typeof categories>;
+export type InsertCategory = InferInsertModel<typeof categories>;
+
+// Categories to Events
 
 export const categoriesToEvents = pgTable(
 	'categories_to_events',
@@ -132,14 +171,17 @@ export const categoriesToEventsRelations = relations(categoriesToEvents, ({ one 
 	})
 }));
 
+// Submissions
+
 export const submissions = pgTable('submission', {
 	id: serial('id').primaryKey(),
-	name: text('name'),
 	userId: text('user_id'),
 	categoryId: integer('category_id'),
 	eventId: integer('event_id'),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	title: text('title'),
+	mediaId: integer('media_id'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -158,16 +200,23 @@ export const submissionsRelations = relations(submissions, ({ one, many }) => ({
 		references: [events.id]
 	}),
 	reactions: many(reactions),
-	votes: many(votes)
+	votes: many(votes),
+	media: one(media, {
+		fields: [submissions.mediaId],
+		references: [media.id]
+	})
 }));
+
+export type Submission = InferSelectModel<typeof submissions>;
+// Votes
 
 export const votes = pgTable('vote', {
 	id: serial('id').primaryKey(),
 	name: text('name'),
 	submissionId: integer('submission_id'),
 	userId: text('user_id'),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -180,13 +229,15 @@ export const votesRelations = relations(votes, ({ one }) => ({
 	})
 }));
 
+// Reactions
+
 export const reactions = pgTable('reaction', {
 	id: serial('id').primaryKey(),
 	value: text('name'),
 	userId: text('user_id'),
 	submissionId: integer('submission_id'),
-	createdAt: timestamp('createdAt').defaultNow(),
-	updatedAt: timestamp('updatedAt')
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
 		.defaultNow()
 		.$onUpdate(() => new Date())
 });
@@ -198,6 +249,27 @@ export const reactionsRelations = relations(reactions, ({ one }) => ({
 	}),
 	submission: one(submissions, {
 		fields: [reactions.submissionId],
+		references: [submissions.id]
+	})
+}));
+
+// Media
+
+export const media = pgTable('media', {
+	id: serial('id').primaryKey(),
+	submissionId: integer('submission_id'),
+	type: mediaTypeEnum('type').notNull(),
+	url: text('url').notNull(),
+	alt: text('alt'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at')
+		.defaultNow()
+		.$onUpdate(() => new Date())
+});
+
+export const mediaRelations = relations(media, ({ one }) => ({
+	submission: one(submissions, {
+		fields: [media.submissionId],
 		references: [submissions.id]
 	})
 }));
