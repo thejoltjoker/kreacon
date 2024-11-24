e006e4724e67a5f76ad265e7dda6e93e7228d99a
Johannes Andersson <hello@thejoltjoker.com>
2024-11-24 20:00:31 +0100

    feat: increment views

--- src/routes/(app)/submissions/[id]/+page.server.ts
+++ src/routes/(app)/submissions/[id]/+page.server.ts
@@ -1,39 +1,44 @@
 import { db } from '$lib/server/db';
 import { submissions } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm/pg-core/expressions';
+
 import type { PageServerLoad } from './$types';
 import { z } from 'zod';
 import reactions, { insertReactionSchema } from '$lib/server/db/schema/reaction';
 import { fail } from '@sveltejs/kit';
+import { eq, sql } from 'drizzle-orm';
 
 export const load = (async ({ params, locals }) => {
 	const { id } = params;
 
-	const result = await db.query.submissions.findFirst({
-		where: eq(submissions.id, id),
-		with: {
-			media: true,
-			category: true,
-			user: {
-				columns: {
-					username: true,
-					picture: true
-				}
-			},
-			reactions: {
-				with: {
-					user: {
-						columns: {
-							username: true,
-							picture: true
+	const result = await db.transaction(async (tx) => {
+		await tx
+			.update(submissions)
+			.set({ views: sql`${submissions.views} + 1` })
+			.where(eq(submissions.id, id));
+
+		return await tx.query.submissions.findFirst({
+			where: eq(submissions.id, id),
+			with: {
+				media: true,
+				category: true,
+				user: {
+					columns: {
+						username: true,
+						picture: true
+					}
+				},
+				reactions: {
+					with: {
+						user: {
+							columns: {
+								username: true,
+								picture: true
+							}
 						}
 					}
 				}
 			}
-			// votes: {
-			// 	where: and(eq(votes.submissionId, id), eq(votes.userId, user!.id))
-			// }
-		}
+		});
 	});
 
 	return { submission: result, user: locals.user };
