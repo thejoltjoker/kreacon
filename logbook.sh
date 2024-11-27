19a169c007f321639e96fb9f9d987ae4d7d9c222
Johannes Andersson <hello@thejoltjoker.com>
2024-11-27 10:06:45 +0100

    test: test

--- logbook.sh
+++ logbook.sh
@@ -1,14 +1,14 @@
 echo "# $(date '+%Y-%m-%d')\n" >> "$1"
-echo "## Commits for $(date '+%Y-%m-%d')\n" >> "$1"
+echo "## Commits\n" >> "$1"
 
 git_log=$(git log --since="midnight" --pretty=format:"- [%h](https://github.com/thejoltjoker/kreacon/commit/%h) - %s")
 echo "Commits for today:"
 echo "$git_log"
 echo "$git_log" >> "$1"
 
-echo "\n## Reflections\n" >> "$1"
+echo "\n## Notes\n" >> "$1"
 
 read -p "Enter your reflections for today: " reflection
 echo "$reflection" >> "$1"
 echo "" >> "$1"
-echo "---" >> "$1"
+echo "---\n" >> "$1"
