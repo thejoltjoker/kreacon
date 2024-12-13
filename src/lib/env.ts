43a7a266c2fd2954b86ccea946ca256f48104b1e
Johannes Andersson <hello@thejoltjoker.com>
2024-12-13 14:15:39 +0100

    fix: use `safeParse` instead when validating env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,9 +12,6 @@ const stringBoolean = z.coerce
 const EnvSchema = z.object({
 	BASE_URL: z.string().url(),
 	DATABASE_URL: z.string(),
-	DB_MIGRATING: stringBoolean,
-	DB_SEEDING: stringBoolean,
-	OAUTH_DISCORD_CLIENT_ID: z.string(),
 	OAUTH_DISCORD_CLIENT_SECRET: z.string(),
 	OAUTH_DISCORD_REDIRECT_URI: z.string(),
 	OAUTH_GITHUB_CLIENT_ID: z.string(),
@@ -22,6 +19,9 @@ const EnvSchema = z.object({
 	OAUTH_GITHUB_REDIRECT_URI: z.string(),
 	TICKET_API_URL: z.string().url(),
 	// Optional
+	DB_MIGRATING: stringBoolean,
+	DB_SEEDING: stringBoolean,
+	OAUTH_DISCORD_CLIENT_ID: z.string(),
 	AZURE_APP_INSIGHTS_CONNECTION_STRING: z.string().optional(),
 	AZURE_STORAGE_ACCOUNT_NAME: z.string().optional(),
 	DB_HOST: z.string().optional(),
@@ -35,20 +35,16 @@ const EnvSchema = z.object({
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
 
-try {
-	EnvSchema.parse(process.env);
-	console.log('Environment is configured correctly');
-} catch (error) {
-	if (error instanceof ZodError) {
-		console.error('Environment validation failed:');
-		for (const issue of error.issues) {
-			console.error(`- ${issue.path[0]}: ${issue.message}`);
-		}
-		process.exit(1);
-	} else {
-		console.error('Unexpected error during environment validation:', error);
-		process.exit(1);
+const result = EnvSchema.safeParse(process.env);
+
+if (!result.success) {
+	console.error('Environment validation failed:');
+	for (const issue of result.error.issues) {
+		console.error(`- ${issue.path[0]}: ${issue.message}`);
 	}
+	process.exit(1);
+} else {
+	console.log('Environment is configured correctly');
 }
 
 export default EnvSchema.parse(process.env);
