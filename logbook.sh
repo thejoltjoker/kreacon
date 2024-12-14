3e3f6422c717fa05602435e14af25a3d09e076e0
Johannes Andersson <hello@thejoltjoker.com>
2024-12-14 22:32:48 +0100

    feat: test admin categories

--- logbook.sh
+++ logbook.sh
@@ -1,7 +1,7 @@
 echo "# $(date '+%Y-%m-%d')\n" >> "$1"
 echo "## Commits\n" >> "$1"
 
-git_log=$(git log --since="midnight" --pretty=format:"- [%h](https://github.com/thejoltjoker/kreacon/commit/%h) - %s")
+git_log=$(git log --since="midnight" --pretty=format:"- [%h](https://github.com/thejoltjoker/kreacon/commit/%h) - %s" --reverse)
 echo "Commits for today:"
 echo "$git_log"
 echo "$git_log" >> "$1"
