44f51bd4cb049c2d590fd809a3a3a4161e8ca214
Johannes <hello@thejoltjoker.com>
2025-12-08 09:19:57 +0100

    chore: uncomment email verification code

--- src/lib/server/auth/verifyEmail.ts
+++ src/lib/server/auth/verifyEmail.ts
@@ -1,28 +1,28 @@
-// import { AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS, JWT_SIGNATURE } from '$env/static/private';
-// import { Email, type EmailMessage } from '$lib/server/services/email';
-// import crypto from 'crypto';
-// import template from '../../../assets/verify-email-template.html?raw';
+import { AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS, JWT_SIGNATURE } from '$env/static/private';
+import { Email, type EmailMessage } from '$lib/server/services/email';
+import crypto from 'crypto';
+import template from '../../../assets/verify-email-template.html?raw';
 
-// export const createVerifyEmailToken = (email: string) => {
-// 	const authString = `${JWT_SIGNATURE}:${email}`;
-// 	return crypto.createHash('sha256').update(authString).digest('hex');
-// };
+export const createVerifyEmailToken = (email: string) => {
+	const authString = `${JWT_SIGNATURE}:${email}`;
+	return crypto.createHash('sha256').update(authString).digest('hex');
+};
 
-// export const createVerifyEmailLink = (email: string): string => {
-// 	const token = createVerifyEmailToken(email);
-// 	const uriEncodedEmail = encodeURIComponent(email);
-// 	const url = `http://${process.env.PUBLIC_BASE_URL ?? 'localhost:5173'}/verify-email/${uriEncodedEmail}/${token}`;
-// 	return url;
-// };
+export const createVerifyEmailLink = (email: string): string => {
+	const token = createVerifyEmailToken(email);
+	const uriEncodedEmail = encodeURIComponent(email);
+	const url = `http://${process.env.PUBLIC_BASE_URL ?? 'localhost:5173'}/verify-email/${uriEncodedEmail}/${token}`;
+	return url;
+};
 
-// export const sendEmailVerification = async (recipient: string) => {
-// 	const verificationLink = createVerifyEmailLink(recipient);
-// 	const emailMessage: EmailMessage = {
-// 		from: AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
-// 		to: recipient,
-// 		subject: 'Verify your email',
-// 		body: template.replace('{{VERIFICATION_LINK}}', verificationLink)
-// 	};
-// 	const email = new Email(emailMessage);
-// 	await email.send();
-// };
+export const sendEmailVerification = async (recipient: string) => {
+	const verificationLink = createVerifyEmailLink(recipient);
+	const emailMessage: EmailMessage = {
+		from: AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
+		to: recipient,
+		subject: 'Verify your email',
+		body: template.replace('{{VERIFICATION_LINK}}', verificationLink)
+	};
+	const email = new Email(emailMessage);
+	await email.send();
+};
--- src/lib/server/services/email.ts
+++ src/lib/server/services/email.ts
@@ -1,127 +1,127 @@
-// import {
-// 	AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
-// 	AZURE_COMMUNICATION_SERVICES_CONNECTION_STRING
-// } from '$env/static/private';
-// import {
-// 	EmailClient as AzureEmailClient,
-// 	type EmailMessage as AzureEmailMessage
-// } from '@azure/communication-email';
-// import { z } from 'zod/v4'
+import {
+	AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
+	AZURE_COMMUNICATION_SERVICES_CONNECTION_STRING
+} from '$env/static/private';
+import {
+	EmailClient as AzureEmailClient,
+	type EmailMessage as AzureEmailMessage
+} from '@azure/communication-email';
+import { z } from 'zod/v4'
 
-// export const emailClient = new AzureEmailClient(AZURE_COMMUNICATION_SERVICES_CONNECTION_STRING);
+export const emailClient = new AzureEmailClient(AZURE_COMMUNICATION_SERVICES_CONNECTION_STRING);
 
-// export type EmailServices = 'azure';
+export type EmailServices = 'azure';
 
-// export type EmailMessage = {
-// 	from: string;
-// 	to: string;
-// 	subject: string;
-// 	body: string;
-// };
+export type EmailMessage = {
+	from: string;
+	to: string;
+	subject: string;
+	body: string;
+};
 
-// export class EmailBase {
-// 	private _from: string = '';
-// 	private _to: string = '';
-// 	private _subject: string = '';
-// 	private _body: string = '';
-// 	public service: EmailServices;
+export class EmailBase {
+	private _from: string = '';
+	private _to: string = '';
+	private _subject: string = '';
+	private _body: string = '';
+	public service: EmailServices;
 
-// 	constructor(service?: EmailServices, message?: EmailMessage) {
-// 		this.service = service ?? 'azure';
-// 		if (message) {
-// 			this._from = message.from;
-// 			this._to = message.to;
-// 			this._subject = message.subject;
-// 			this._body = message.body;
-// 		}
-// 	}
+	constructor(service?: EmailServices, message?: EmailMessage) {
+		this.service = service ?? 'azure';
+		if (message) {
+			this._from = message.from;
+			this._to = message.to;
+			this._subject = message.subject;
+			this._body = message.body;
+		}
+	}
 
-// 	fromObject(email: { from: string; to: string; subject: string; body: string }) {
-// 		this._from = email.from;
-// 		this._to = email.to;
-// 		this._subject = email.subject;
-// 		this._body = email.body;
-// 	}
+	fromObject(email: { from: string; to: string; subject: string; body: string }) {
+		this._from = email.from;
+		this._to = email.to;
+		this._subject = email.subject;
+		this._body = email.body;
+	}
 
-// 	static validateAddress(address: string) {
-// 		try {
-// 			z.string().email().parse(address);
-// 			return true;
-// 		} catch (err) {
-// 			console.warn(err);
-// 			return false;
-// 		}
-// 	}
+	static validateAddress(address: string) {
+		try {
+			z.string().email().parse(address);
+			return true;
+		} catch (err) {
+			console.warn(err);
+			return false;
+		}
+	}
 
-// 	set from(address: string) {
-// 		if (EmailBase.validateAddress(address)) {
-// 			this._from = address;
-// 		}
-// 	}
+	set from(address: string) {
+		if (EmailBase.validateAddress(address)) {
+			this._from = address;
+		}
+	}
 
-// 	get from(): string {
-// 		return this._from;
-// 	}
+	get from(): string {
+		return this._from;
+	}
 
-// 	set to(address: string) {
-// 		if (EmailBase.validateAddress(address)) {
-// 			this._to = address;
-// 		}
-// 	}
+	set to(address: string) {
+		if (EmailBase.validateAddress(address)) {
+			this._to = address;
+		}
+	}
 
-// 	get to(): string {
-// 		return this._to;
-// 	}
+	get to(): string {
+		return this._to;
+	}
 
-// 	set subject(subject: string) {
-// 		this._subject = subject;
-// 	}
+	set subject(subject: string) {
+		this._subject = subject;
+	}
 
-// 	get subject(): string {
-// 		return this._subject;
-// 	}
+	get subject(): string {
+		return this._subject;
+	}
 
-// 	set body(body: string) {
-// 		this._body = body;
-// 	}
+	set body(body: string) {
+		this._body = body;
+	}
 
-// 	get body(): string {
-// 		return this._body;
-// 	}
+	get body(): string {
+		return this._body;
+	}
 
-// 	async _sendAzure() {
-// 		const emailMessage: AzureEmailMessage = {
-// 			senderAddress: AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
-// 			content: {
-// 				subject: this.subject,
-// 				html: this.body
-// 			},
-// 			recipients: {
-// 				to: [{ address: this.to }]
-// 			}
-// 		};
+	async _sendAzure() {
+		const emailMessage: AzureEmailMessage = {
+			senderAddress: AZURE_COMMUNICATION_SERVICES_SENDER_ADDRESS,
+			content: {
+				subject: this.subject,
+				html: this.body
+			},
+			recipients: {
+				to: [{ address: this.to }]
+			}
+		};
 
-// 		const poller = await emailClient.beginSend(emailMessage);
-// 		const result = await poller.pollUntilDone();
+		const poller = await emailClient.beginSend(emailMessage);
+		const result = await poller.pollUntilDone();
 
-// 		return result;
-// 	}
+		return result;
+	}
 
-// 	async send() {
-// 		switch (this.service) {
-// 			case 'azure':
-// 				return this._sendAzure();
-// 			default:
-// 				throw new Error('Invalid email service');
-// 		}
-// 	}
-// }
+	async send() {
+		switch (this.service) {
+			case 'azure':
+				return this._sendAzure();
+			default:
+				throw new Error('Invalid email service');
+		}
+	}
+}
 
-// export class AzureEmail extends EmailBase {
-// 	constructor(message?: EmailMessage) {
-// 		super('azure', message);
-// 	}
-// }
+export class AzureEmail extends EmailBase {
+	constructor(message?: EmailMessage) {
+		super('azure', message);
+	}
+}
 
-// // Set the general email service
-// export const Email = AzureEmail;
+// Set the general email service
+export const Email = AzureEmail;
