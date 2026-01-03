ca0e81c152ac84b3d448bbac9f73d4a9ffcc5039
Johannes <hello@thejoltjoker.com>
2026-01-03 22:25:16 +0100

    fix: reinit Sentry integration

---
+++ src/hooks.client.ts
@@ -0,0 +1,14 @@
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: '',
+	tracesSampleRate: import.meta.env.DEV ? 1.0 : 1.0,
+	enableLogs: true,
+	replaysSessionSampleRate: import.meta.env.DEV ? 1.0 : 1.0,
+	replaysOnErrorSampleRate: import.meta.env.DEV ? 1.0 : 1.0,
+	integrations: [replayIntegration()],
+	sendDefaultPii: true
+});
+// TODO If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
