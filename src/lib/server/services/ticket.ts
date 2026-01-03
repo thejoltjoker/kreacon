7167dd63dc3eacb1637058a7046096b45ec4fcc4
Johannes <hello@thejoltjoker.com>
2026-01-03 12:50:23 +0100

    feat: enhance ticket validation logic with temporary debug response and API integration

--- src/lib/server/services/ticket.ts
+++ src/lib/server/services/ticket.ts
@@ -1,14 +1,45 @@
 import env from '$lib/env';
+import z from 'zod/v4';
 
 export interface ValidatedTicket {
 	id: string;
 	event: string;
+	slug: string;
 }
+
 export const api: {
 	validate: (ticketId: string) => Promise<ValidatedTicket | undefined>;
 } = {
 	validate: async (ticketId: string) => {
-		const response = await fetch(`${env.TICKET_API_URL}/${ticketId}`);
+		// TODO remove this after beacon 2026 event
+		if (z.uuid().safeParse(ticketId).success) {
+			return {
+				id: ticketId,
+				event: 'Debug Derby',
+				slug: 'debug-derby'
+			};
+		}
+
+		const headers: HeadersInit = {
+			'Content-Type': 'application/json'
+		};
+
+		if (env.TICKET_API_SECRET && env.TICKET_API_SECRET !== '') {
+			headers['Authorization'] = `Bearer ${env.TICKET_API_SECRET}`;
+		}
+
+		const response = await fetch(`${env.TICKET_API_URL}/tickets/validate`, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify({
+				bookingNumber: ticketId
+			})
+		});
+
+		if (!response.ok) {
+			return undefined;
+		}
+
 		const data: ValidatedTicket = await response.json();
 		return data;
 	}
