43a9ca75bada27dd5418dd0911a873a36f27b047
Johannes <hello@thejoltjoker.com>
2026-01-04 00:28:52 +0100

    chore: standardize Sentry configuration formatting in client and server hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,28 +1,29 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://f96344f191d1b3836c57c023b8aabb10@o4510613286551552.ingest.de.sentry.io/4510613288190032',
+	dsn: 'https://f96344f191d1b3836c57c023b8aabb10@o4510613286551552.ingest.de.sentry.io/4510613288190032',
+	// tunnel: '/tunnel',
 
-  tracesSampleRate: 1.0,
+	tracesSampleRate: 1.0,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+	// Enable logs to be sent to Sentry
+	enableLogs: true,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
 
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration()],
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [replayIntegration()],
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+	// Enable sending user PII (Personally Identifiable Information)
+	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
+	sendDefaultPii: true
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,13 +1,13 @@
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://f96344f191d1b3836c57c023b8aabb10@o4510613286551552.ingest.de.sentry.io/4510613288190032',
+	dsn: 'https://f96344f191d1b3836c57c023b8aabb10@o4510613286551552.ingest.de.sentry.io/4510613288190032',
 
-  tracesSampleRate: 1.0,
+	tracesSampleRate: 1.0,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+	// Enable logs to be sent to Sentry
+	enableLogs: true
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
 });
